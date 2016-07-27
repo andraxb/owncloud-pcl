3d31fc1bd40414a1b248f71800d4c51591c73802
AndraxB <andraxb@gmail.com>
2016-07-27 16:21:35 +0200

    Unit test project for iOS added.

---
+++ owncloud-pcl-ios-unit-tests/Main.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------
+// --------- Made By Andrax ----------------
+// andrax@andrax.nl  |  http://www.andrax.nl 
+// github.com/andraxb
+// Copyright (c) Andrax
+// -----------------------------------------
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+using Foundation;
+using UIKit;
+
+namespace owncloudpcliosunittests
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			// if you want to use a different Application Delegate class from "UnitTestAppDelegate"
+			// you can specify it here.
+			UIApplication.Main (args, null, "UnitTestAppDelegate");
+		}
+	}
+}
---
+++ owncloud-pcl-ios-unit-tests/OwnCloudClientTests.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace owncloudpcliosunittests
+{
+	// TODO
+
+	/// <summary>
+	/// Own cloud client tests.
+	/// </summary>
+	[TestFixture]
+	public class OwnCloudClientTests
+	{
+		[Test]
+		public void Pass ()
+		{
+			Assert.True (true);
+		}
+
+		[Test]
+		public void Fail ()
+		{
+			Assert.False (true);
+		}
+
+		[Test]
+		[Ignore ("another time")]
+		public void Ignore ()
+		{
+			Assert.True (false);
+		}
+	}
+}
