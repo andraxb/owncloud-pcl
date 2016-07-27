389489d3aebc62a054889f9911941ebb7305fafa
AndraxB <andraxb@gmail.com>
2016-07-27 15:31:40 +0200

    Initial Solution and project ocmmit

---
+++ owncloud-pcl/OwnCloudClient.cs
@@ -0,0 +1,18 @@
+﻿// -----------------------------------------
+// --------- Made By Andrax ----------------
+// andrax@andrax.nl  |  http://www.andrax.nl 
+// github.com/andraxb
+// Copyright (c) Andrax
+// -----------------------------------------
+using System;
+
+namespace owncloudpcl
+{
+	public class OwnCloudClient
+	{
+		public OwnCloudClient ()
+		{
+		}
+	}
+}
+
---
+++ owncloud-pcl/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------
+// --------- Made By Andrax ----------------
+// andrax@andrax.nl  |  http://www.andrax.nl 
+// github.com/andraxb
+// Copyright (c) Andrax
+// -----------------------------------------
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("owncloud-pcl")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Andrax")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("Andrax")]
+[assembly: AssemblyTrademark ("Andrax")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
